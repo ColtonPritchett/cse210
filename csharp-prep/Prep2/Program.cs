bad58e9dfe0b57cd32638f255b2b14735d6ed851
Colton Pritchett <prit22019@byui.edu>
2023-01-07 17:07:34 -0700

    Finished the assignment with some extra features

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        bool seguir = true;
+        while(seguir){
+            Console.WriteLine("What was your grade? ");
+            int grade = int.Parse(Console.ReadLine());
+            string letter;
+            if(grade >= 90){
+                letter = "A";
+            } else if(grade >= 80){
+                letter = "B";
+            } else if(grade >= 70){
+                letter = "C";
+            } else if(grade >= 60){
+                letter = "D";
+            } else {
+                letter = "F";
+            }
+            if(grade % 10 >= 7 && grade <= 90){
+                letter = letter + "+";
+            } else if(grade % 10 <= 3){
+                letter = letter + "-";
+            }
+            Console.WriteLine($"Letter grade: {letter}");
+            Console.WriteLine("Do you wish to continue (y/n)? ");
+            string answer = Console.ReadLine();
+            if(answer == "n") seguir = false;
+        }
     }
 }
