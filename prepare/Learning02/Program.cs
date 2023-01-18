6eeb1f92001a4ca61d0c65c2fd29cc62dcb5bc2c
ColtonPritchett <121995308+ColtonPritchett@users.noreply.github.com>
2023-01-18 15:29:47 -0700

    Completed the Abstraction Learning Activity

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job("Prairie Falls Golf Club", "Landscaper", 2018, 2018);
+        Job job2 = new Job("Gun Fighters Inc.", "Assembly Line Worker", 2019, 2022);
+        List<Job> jobs = new List<Job>();
+        jobs.Add(job1);
+        jobs.Add(job2);
+        Resume resume = new Resume("Colton Pritchett", jobs);
+        resume.Display();
+    }
+}
+
+public class Job{
+    string _company, _jobTitle, _position;
+    int _startYear, _endYear;
+    public Job(string company, string jobTitle, int startYear, int endYear){
+        _company = company;
+        _jobTitle = jobTitle;
+        _startYear = startYear;
+        _endYear = endYear;
+        _position = _jobTitle + " (" + _company + ") " + _startYear + "-" + _endYear;
+    }
+
+    public void Display(){
+        Console.WriteLine(_position);
+    }
+}
+
+public class Resume{
+    string _name;
+    List<Job> _jobs;
+
+    public Resume(string name, List<Job> jobs){
+        _name = name;
+        _jobs = jobs;
+    }
+
+    public void Display(){
+        Console.WriteLine("Name: " + _name);
+        Console.WriteLine("Jobs:");
+
+        foreach(Job i in _jobs){
+            i.Display();
+        }
     }
 }
