bec73f874294357cf160a90116deeec0de8cb2ed
ColtonPritchett <121995308+ColtonPritchett@users.noreply.github.com>
2023-02-09 14:59:07 -0700

    did some stuff

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction = new Fraction();
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetTop());
+        fraction.SetTop(5);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        fraction.Set(3, 4);
+        Console.WriteLine(fraction.GetFractionString());
+        Console.WriteLine(fraction.GetDecimalValue());
+        fraction.SetBottom(2);
+        Console.WriteLine(fraction.GetBottom());
+    }
+}
+
+class Fraction{
+    private int top;
+    private int bottom;
+
+    public Fraction(){
+        top = 1;
+        bottom = 1;
+    }
+
+    public Fraction(int _top, int _bottom){
+        top = _top;
+        bottom = _bottom;
+    }
+
+    public int GetTop(){
+        return top;
+    }
+
+    public void SetTop(int _top){
+        top = _top;
+    }
+
+    public int GetBottom(){
+        return bottom;
+    }
+
+    public void SetBottom(int _bottom){
+        bottom = _bottom;
+    }
+
+    public void Set(int _top, int _bottom){
+        top = _top;
+        bottom = _bottom;
+    }
+
+    public string GetFractionString(){
+        return "" + top + "/" + bottom;
+    }
+
+    public double GetDecimalValue(){
+        return (double) top / bottom;
     }
 }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,31 +1,40 @@
 using System;
 using System.Text.Json;
 
+namespace DeserializeExtra{
 class Program
 {
     static void Main(string[] args)
     {
-        Book bom;
-        using (StreamReader r = new StreamReader("book-of-mormon-flat.json")) {  
-            string json = r.ReadToEnd();  
-            bom = JsonSerializer.Deserialize<Book>(json);  
-        }
+        List<Library> bom = new List<Library>();
+        bom.Add(JsonSerializer.Deserialize<Library>(new StreamReader("old-testament.json").ReadToEnd()));
+        bom.Add(JsonSerializer.Deserialize<Library>(new StreamReader("new-testament.json").ReadToEnd()));
+        bom.Add(JsonSerializer.Deserialize<Library>(new StreamReader("book-of-mormon.json").ReadToEnd()));
+        bom.Add(JsonSerializer.Deserialize<Library>(new StreamReader("doctrine-and-covenants.json").ReadToEnd()));
+        bom.Add(JsonSerializer.Deserialize<Library>(new StreamReader("pearl-of-great-price.json").ReadToEnd()));
+
+        
     }
 }
 
-class Verses{
-    public string reference{get; set;}
-    public string text{get; set;}
-
-    public Verses(){
-
-    }
-    public Verses(string Reference, string Text){
-        reference = Reference;
-        text = Text;
-    }
+public class Library{
+    public IList<Book>? books{get; set;}
 }
 
-class Book{
-    List<Verses> verses{get; set;}
+public class Book{
+    public string? book{get; set;}
+    public IList<Chapter>? chapters{get; set;}
+}
+
+public class Chapter{
+    public int? chapter{get; set;}
+    public string? reference{get; set;}
+    public IList<Verse>? verses{get; set;}
+}
+
+public class Verse{
+    public string? reference{get; set;}
+    public string? text{get; set;}
+    public int? verse{get; set;}
+}
 }
