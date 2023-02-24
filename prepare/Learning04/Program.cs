909e2a2f5a7c57f7814b31a97f5900ae22ecb25b
ColtonPritchett <121995308+ColtonPritchett@users.noreply.github.com>
2023-02-24 15:58:17 -0700

    I did the assignment

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment ma = new MathAssignment("Colton Pritchett", "Fractions", "7.3", "8-19");
+        WritingAssignment wa = new WritingAssignment("Colton Pritchett", "European History", "The Causes of World War II");
+        Console.WriteLine(ma.GetSummary() + "\n" + ma.GetHomeworkList() + "\n\n" + wa.GetSummary() + "\n" + wa.GetWritingInformation());
     }
 }
